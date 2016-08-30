a7a552c6915cc332d8fae834af506e30515b7ba0
sbhat <sbhat@fnal.gov>
2016-08-30 09:28:17 -0500

    Added new basicsleep script with ifdhc file transfer test

---
+++ basicsleep_ft.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ $# -gt 0 ] 
+then	
+	time=$1
+else
+	time=5
+fi
+
+echo "Going to sleep for $time seconds"
+
+sleep $time 
+
+echo "Woke up!"
+
+echo "Now testing file transfer"
+
+
+
+touch /tmp/a
+
+source /grid/fermiapp/products/common/etc/setups.sh
+setup ifdhc
+
+ls /pnfs/dune/scratch/users/sbhat/a > /dev/null $2>$1
+
+if [ $? == 0 ]
+then
+	echo "REMOVING A"
+	rm -f /pnfs/dune/scratch/users/sbhat/a
+fi
+
+ifdh cp -D /tmp/a /pnfs/dune/scratch/users/sbhat/
